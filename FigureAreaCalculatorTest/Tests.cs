cd88dfe41afc51945ddee9e56c2b9545d5ec1244
Bal4ss <wwwnikita18@gmail.com>
2021-07-21 02:38:40 +0500

    Feat: rework nunit tests

--- FigureAreaCalculatorTest/Tests.cs
+++ FigureAreaCalculatorTest/Tests.cs
@@ -23,35 +23,49 @@ namespace FigureAreaCalculatorTest
         [TestCase(new double[] { -1, -2, -6 })]
         public void CreateFailTriangleTest(double[] incorrectValues)
         {
-            _triangle = new Triangle(
-                a: incorrectValues[0],
-                b: incorrectValues[1],
-                c: incorrectValues[2]);
-            Assert.Fail();
+            try
+            {
+                _triangle = new Triangle(
+                    a: incorrectValues[0],
+                    b: incorrectValues[1],
+                    c: incorrectValues[2]);
+                Assert.Fail("No exception was thrown");
+            }
+            catch (AggregateException ex)
+            {
+                Assert.AreEqual(
+                    new AggregateException("Impossible triangle side lengths"),
+                    ex);
+            }
         }
 
         [Test]
         [TestCase(new double[] { 5, 4, 6 }, 9.921567)]
         [TestCase(new double[] { -5, -4, -6 }, 9.921567)]
-        public void TriangleAreaTest(double[] correctValues, double resultValue)
+        public void TriangleAreaTest(double[] correctValues, double expected)
         {
             _triangle = new Triangle(
                 a: correctValues[0],
                 b: correctValues[1],
                 c: correctValues[2]);
             var result = _triangle.Area();
-            Assert.Equals(result, resultValue);
+            Assert.AreEqual(expected, result, 0.0001);
         }
-
+        
         [Test]
-        [TestCase(0, 0)]
-        [TestCase(4, 50.26548245743669)]
-        [TestCase(4, 78.53981633974483)]
-        public void CircleAreaTest(double radius, double resultValue)
+        [TestCase(0)]
+        [TestCase(4)]
+        [TestCase(5)]
+        [TestCase(-5)]
+        public void CircleAreaTest(double radius)
         {
             _circle = new Circle(radius);
             var result = _circle.Area();
-            Assert.Equals(result, resultValue);
+            
+            radius = radius > 0 ? radius : -radius;
+            var expected = Math.PI * radius * radius; // S=πr^2
+            
+            Assert.AreEqual(expected, result, 0.0001);
         }
     }
 }
